011e3b376437d1cd88ec2e028d5d0206d3574e7d
2207114 <fokrul2207114@stud.kuet.ac.bd>
2025-11-17 00:04:23 +0600

    updated hello controller

--- src/main/java/com/example/_207114/HelloController.java
+++ src/main/java/com/example/_207114/HelloController.java
@@ -1,14 +1,25 @@
 package com.example._207114;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button create;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void oncreate() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("form.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        Stage stage = (Stage) create.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
