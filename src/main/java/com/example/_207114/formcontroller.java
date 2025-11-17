4bab7d6d7460eb6864fe251d77e8d52daa8ac81e
2207114 <fokrul2207114@stud.kuet.ac.bd>
2025-11-17 16:12:37 +0600

    updated form controller

--- src/main/java/com/example/_207114/formcontroller.java
+++ src/main/java/com/example/_207114/formcontroller.java
@@ -1,4 +1,25 @@
 package com.example._207114;
 
-public class formcontroller {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class  formcontroller{
+    @FXML
+    private Button genrate;
+
+    @FXML
+    public void ongenerate() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("preview.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        Stage stage = (Stage) genrate.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
