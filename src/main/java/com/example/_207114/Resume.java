3437e84deef0935c080aca74a0c171a66a6a246f
2207114 <fokrul2207114@stud.kuet.ac.bd>
2025-11-25 23:25:47 +0600

    added dependies and build resume

---
+++ src/main/java/com/example/_207114/Resume.java
@@ -0,0 +1,144 @@
+package com.example._207114;
+public class Resume {
+    private int id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String country;
+    private String division;
+    private String city;
+    private String house;
+    private String ssc;
+    private String hsc;
+    private String bsc;
+    private String msc;
+    private String skills;
+    private String experience;
+    private String projects;
+    private String imagePath;
+    public Resume() {}
+    public Resume(String fullName, String email, String phone, String country,
+                  String division, String city, String house, String ssc, String hsc,
+                  String bsc, String msc, String skills, String experience,
+                  String projects, String imagePath) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.country = country;
+        this.division = division;
+        this.city = city;
+        this.house = house;
+        this.ssc = ssc;
+        this.hsc = hsc;
+        this.bsc = bsc;
+        this.msc = msc;
+        this.skills = skills;
+        this.experience = experience;
+        this.projects = projects;
+        this.imagePath = imagePath;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getDivision() {
+        return division;
+    }
+    public void setDivision(String division) {
+        this.division = division;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getHouse() {
+        return house;
+    }
+    public void setHouse(String house) {
+        this.house = house;
+    }
+    public String getSsc() {
+        return ssc;
+    }
+    public void setSsc(String ssc) {
+        this.ssc = ssc;
+    }
+    public String getHsc() {
+        return hsc;
+    }
+    public void setHsc(String hsc) {
+        this.hsc = hsc;
+    }
+    public String getBsc() {
+        return bsc;
+    }
+    public void setBsc(String bsc) {
+        this.bsc = bsc;
+    }
+    public String getMsc() {
+        return msc;
+    }
+    public void setMsc(String msc) {
+        this.msc = msc;
+    }
+    public String getSkills() {
+        return skills;
+    }
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+    public String getExperience() {
+        return experience;
+    }
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+    public String getProjects() {
+        return projects;
+    }
+    public void setProjects(String projects) {
+        this.projects = projects;
+    }
+    public String getImagePath() {
+        return imagePath;
+    }
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    @Override
+    public String toString() {
+        return "Resume{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
